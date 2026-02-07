6106f4829a8d95507210e5dbc2d6c04be1eb8c79
Gus-oss <gustavo223@live.com.mx>
2026-02-07 16:19:29 -0600

    Gus- añadi la carpeta de imagenes y su funcion

--- Codigos/main.py
+++ Codigos/main.py
@@ -1,8 +1,21 @@
-#Librerias importantes
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#Librerias 
 import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Configuración de la carpeta para guardar imágenes
+IMAGES_PATH = "Galeria"  # Nombre de la carpeta
+os.makedirs(IMAGES_PATH, exist_ok=True) # Crea la carpeta si no existe
 
+def save_fig(fig_id, tight_layout=True, fig_extension="pdf", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Imagen guardada como ", fig_id, "en la carpeta", IMAGES_PATH) #Mensaje mas informativo
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 print("Hola mundo")
